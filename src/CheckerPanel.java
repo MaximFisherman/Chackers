bbe11501805b71b397451fe8d684f6aaae2a1ecd
Maxim <maksimrybak666@gmail.com>
2016-09-24 16:10:02 +0300

    This is my second ficsation

--- src/CheckerPanel.java
+++ src/CheckerPanel.java
@@ -225,7 +225,7 @@ public class CheckerPanel extends JPanel implements MouseMotionListener, MouseLi
 		g.fillRect(0, 0, 660, 700);
 		g.setColor(Color.lightGray);
 		g.fillRect(660, 0, 700, 700);		
-		g.setColor(Color.yellow);
+		g.setColor(Color.red);
 		g.fillRect(2, 26, 80, 80);
 		g.fillRect(162, 26, 80, 80);
 		g.fillRect(322, 26, 80, 80);
@@ -267,7 +267,7 @@ public class CheckerPanel extends JPanel implements MouseMotionListener, MouseLi
 		g.fillRect(402, 586, 80, 80);
 		g.fillRect(562, 586, 80, 80);
 		//=====================================
-		g.setColor(Color.blue);
+		g.setColor(Color.yellow);
 		checker.checkerBlue11(g);
 		checker.checkerBlue12(g);
 		checker.checkerBlue13(g);
@@ -281,7 +281,7 @@ public class CheckerPanel extends JPanel implements MouseMotionListener, MouseLi
 		checker.checkerBlue33(g);
 		checker.checkerBlue34(g);
 		
-		g.setColor(Color.red);
+		g.setColor(Color.white);
 		checker.checkerWhite11(g);
 		checker.checkerWhite12(g);
 		checker.checkerWhite13(g);
